3ea2cf6b0db5446d38b83be1a6d903cab259408c
Rahim_Badsa <rahimbadsa723@gmail.com>
2025-12-14 13:01:02 +0600

    updated bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red-300`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
         {children}
       </body>
     </html>
